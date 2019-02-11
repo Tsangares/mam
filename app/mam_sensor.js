b39c465ca6ded39d14b05fca64a97f472e1c7102
Kristofvdj88 <kristofvanderjonckheyd@gmail.com>
2019-02-11 20:49:13 +0100

    test

--- app/mam_sensor.js
+++ app/mam_sensor.js
@@ -1,7 +1,7 @@
 const Mam = require('../lib/mam.node.js')
 const IOTA = require('iota.lib.js');
 const Config = require('./config/config');
-//const Gpio = require('pigpio').Gpio;
+const Gpio = require('pigpio').Gpio;
 const Moment = require('moment');
 
 console.log("Config :", Config);

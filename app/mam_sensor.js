1d090990af1c17137ef330e730ccef302561c696
Kristofvdj88 <kristofvanderjonckheyd@gmail.com>
2019-02-13 20:41:27 +0100

    test

--- app/mam_sensor.js
+++ app/mam_sensor.js
@@ -1,19 +1,20 @@
 // TODO : 
 // - Maken the config a json file
-const Mam = require('../lib/mam.client.js')
-const { asciiToTrytes, trytesToAscii } = require('@iota/converter')
-const Config = require('./config/config');
-const Gpio = require('pigpio').Gpio;
-const Moment = require('moment');
+const Mam = require("../lib/mam.client.js")
+const { asciiToTrytes, trytesToAscii } = require("@iota/converter")
+const Config = require("./config/config");
+const Gpio = require("pigpio").Gpio;
+const Moment = require("moment");
 
 let attaching = false;
 let distanceBuffer = 0;
+let counter = 0;
 
 // Initialise MAM State
 let mamState = Mam.init(Config.PROVIDER);
 
 // Set channel mode
-mamState = Config.CHANNELMODE == 'restricted' ? Mam.changeMode(mamState, Config.CHANNELMODE, Config.AUTHORISATION_KEY) : Mam.changeMode(mamState, Config.CHANNELMODE);
+mamState = Config.CHANNELMODE == "restricted" ? Mam.changeMode(mamState, Config.CHANNELMODE, Config.AUTHORISATION_KEY) : Mam.changeMode(mamState, Config.CHANNELMODE);
 
 // Initialise GPIO module
 const trigger = new Gpio(Config.GPIO_TRIGGER_PIN, {mode: Gpio.OUTPUT});
@@ -22,7 +23,7 @@ trigger.digitalWrite(0); // Make sure trigger is low
 
 // Publish to tangle
 const publish = async (packet) => {
-    console.info('publish');
+    console.info("publish");
         
     // Create MAM Payload
     const trytes = asciiToTrytes(JSON.stringify(packet));
@@ -30,44 +31,44 @@ const publish = async (packet) => {
 
     // Save new mamState
     mamState = message.state;
-    console.log('Root: ', message.root);
-    console.log('Address: ', message.address);
+    console.info("Root: ", message.root);
+    console.info("Address: ", message.address);
 
     // Attach the payload.
     await Mam.attach(message.payload, message.address, 3, 9);
 
-    console.log('Published: ', packet, '\n');
+    console.info("Published: ", packet, "\n");
     return message.root;
 };
 
 const readSensor = async () => {
     try {
-        console.info('readSensor');
+        console.info("readSensor");
         attaching = true;
         let startTick;
-        await echo.on('alert', async (level, tick) => {
-            console.info('sensor alert');
+        await echo.on("alert", async (level, tick) => {
+            console.info("sensor alert");
 
             if (level == 1) 
                 startTick = tick;
             else {
                 const endTick = tick;
                 const distance = ((endTick >> 0) - (startTick >> 0)) / 2 / (1e6/34321);
+                console.info();
                 if (persistentChangeDetected(distance)) {
                     const hasMail = distance <= 3;
-                    const dateTime = Moment().utc().format('DD/MM/YYYY hh:mm');
+                    const dateTime = Moment().utc().format("DD/MM/YYYY hh:mm");
                     const json = {
                         "distance": distance,
                         "hasMail": hasMail,
                         "dateTime": dateTime
                     };
                     const root = await publish(json);        
-                    console.log(`dateTime: ${json.dateTime}, distance: ${json.distance}, root: ${root}`);
                 }                
             }
         });
     } catch (e) {
-        console.error('readSensor error: ', e.message);
+        console.error("readSensor error: ", e.message);
     } finally {
         attaching = false;
     }
@@ -76,13 +77,15 @@ const readSensor = async () => {
 const persistentChangeDetected = (distance) => {
     let result = (distanceBuffer <= 3) != (distance <= 3);
     console.info("persistentChangeDetected: ", result);
+    console.info("distance: ", distance);
     distanceBuffer = distance;
     return result;
 };
 
 const triggerSensor = async () => {
-    console.info('triggerSensor');
-
+    console.info("triggerSensor");
+    console.info("Interval ", count);
+    
     if (Config.ENABLED)
         trigger.trigger(10, 1);
     else {
@@ -92,6 +95,7 @@ const triggerSensor = async () => {
 };
 
 readSensor();
+triggerSensor()
 
 // Set a time interval between the reads
 const interval = setInterval(() => { if(!attaching) { triggerSensor() } }, Config.TIME_INTERVAL * 1000);
